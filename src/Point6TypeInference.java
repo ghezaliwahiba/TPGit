340d19ed53535a8d9a798ba3ece357d9ddb3781e
ghezaliwahiba <ghezaliwahiba@yahoo.fr>
2023-12-11 11:56:23 +0100

    Commit Difference var et Final

---
+++ src/Point6TypeInference.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+public class Point6TypeInference {
+
+        public static void main(String[] args) {
+            var input=new Scanner(System.in);
+            var firstName="Christian";
+            var age=12;
+            var gender='F';
+            var weight=56.3;
+        }
+}
---
+++ src/Point7Math.java
@@ -0,0 +1,5 @@
+public class Point7Math {
+    public static void main(String[] args) {
+
+    }
+}
