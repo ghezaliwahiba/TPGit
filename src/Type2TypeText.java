0334a5e7ac065ae0dc1b27b7547f811539588368
ghezaliwahiba <ghezaliwahiba@yahoo.fr>
2023-12-11 10:46:05 +0100

    Commit TypeText

--- src/Type2TypeText.java
+++ src/Type2TypeText.java
@@ -1,5 +1,12 @@
 public class Type2TypeText {
     public static void main(String[] args) {
+        String firstName="Christian";
+        String lastName="Lisangola";
 
+        System.out.println(firstName);
+        System.out.println(lastName);
+
+        char gender='M';
+        System.out.println(gender);
     }
 }
