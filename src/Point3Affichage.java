51871875aa3d15d44673d0f73730b7d56b6805fe
ghezaliwahiba <ghezaliwahiba@yahoo.fr>
2023-12-11 11:22:41 +0100

    commit mode d'affichage

---
+++ src/Point3Affichage.java
@@ -0,0 +1,12 @@
+import java.sql.SQLOutput;
+
+public class Point3Affichage {
+    public static void main(String[] args) {
+        String firstName="Wahiba";
+        String lastName="GHEZALI RAMDANI";
+        int age=38;
+        double weight=50.1;
+        System.out.println("Je m'appelle "+firstName+" "+lastName+", j'ai "+age+" ans, je pèse "+weight+"Kg");
+        System.out.printf("je m'apelle %s %s, j'ai %d et je pese %f", firstName, lastName, age, weight );
+    }
+}
